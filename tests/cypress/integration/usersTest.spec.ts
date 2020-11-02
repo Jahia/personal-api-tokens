2386ba161f2ddd66dcc132783dc0677225a507bc
Vladislav Radan <vradan@jahia.com>
2020-11-02 12:38:20 -0500

    fixed linting issues

--- tests/cypress/integration/usersTest.spec.ts
+++ tests/cypress/integration/usersTest.spec.ts
@@ -4,8 +4,4 @@ describe('navigation to user', () => {
     it('navigates to the users page successfully', function () {
         users.goTo()
     })
-
-    it('navigates to the user anne successfully', function () {
-        users.goTo().editUser('anne')
-    })
 })
