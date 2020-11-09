29373461262c3d121f624325511bfc616e89c548
Thomas Draier <tdraier@jahia.com>
2020-11-09 17:03:21 +0100

    Fixed imports

--- src/main/java/org/jahia/modules/apitokens/graphql/GraphQLPersonalApiTokenExtensionsProvider.java
+++ src/main/java/org/jahia/modules/apitokens/graphql/GraphQLPersonalApiTokenExtensionsProvider.java
@@ -18,20 +18,10 @@ package org.jahia.modules.apitokens.graphql;
 import org.jahia.modules.graphql.provider.dxm.DXGraphQLExtensionsProvider;
 import org.osgi.service.component.annotations.Component;
 
-import java.util.Arrays;
-import java.util.Collection;
-
 /**
  * GraphQLPersonalApiTokenExtensionsProvider
  * Uses DXGraphQLExtensionsProvider class scanning
  */
 @Component(immediate = true)
 public class GraphQLPersonalApiTokenExtensionsProvider implements DXGraphQLExtensionsProvider {
-    @Override
-    public Collection<Class<?>> getExtensions() {
-        return Arrays.asList(
-                GqlPersonalApiTokensMutation.class,
-                GqlPersonalApiTokensQuery.class
-        );
-    }
 }
