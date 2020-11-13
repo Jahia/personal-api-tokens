b28a7ba9587639263de24c1e20a805366edb39a9
Francois Gerthoffert <f.gerthoffert@gmail.com>
2020-11-13 12:02:46 -0500

    Added more tests to createToken

--- tests/cypress/integration/api/createToken.spec.ts
+++ tests/cypress/integration/api/createToken.spec.ts
@@ -1,7 +1,15 @@
 import { apolloClient } from '../../support/apollo'
+import { DocumentNode } from 'graphql'
+
 import { createToken, getToken, getTokens, deleteToken, verifyToken } from '../../support/gql'
 
-describe('token creation', () => {
+describe('Token creation via API - mutation.admin.personalApiTokens.createToken', () => {
+    let GQL_CREATE: DocumentNode
+
+    before('load graphql file', function () {
+        GQL_CREATE = require(`graphql-tag/loader!../../fixtures/createToken.graphql`)
+    })
+
     afterEach(async function () {
         const client = apolloClient()
         return Promise.all(
@@ -11,23 +19,49 @@ describe('token creation', () => {
         )
     })
 
-    it('should create a simple token', async function () {
+    it('Create token by providing userId, name, null date, null state, null siteKey', async function () {
         const client = apolloClient()
         const name = 'test-' + new Date().getTime()
-        const token = await createToken('root', name, null, null, client)
 
+        const response = await apolloClient().query({
+            query: GQL_CREATE,
+            variables: {
+                userId: 'root',
+                tokenName: name,
+                siteKey: null,
+                expireAt: null,
+                tokenState: null,
+            },
+        })
+        expect(response.errors).to.be.undefined
+
+        const token = response.data.admin.personalApiTokens.createToken
         expect(token).to.have.length.of.at.least(10, 'not a token')
         expect(await verifyToken(token, client)).to.be.true
 
         const tokenDetails = await getToken('root', name, client)
         expect(tokenDetails).to.not.be.null
         expect(tokenDetails.name).to.equals(name)
+        expect(tokenDetails.state).to.equals('ACTIVE')
     })
 
-    it('should create a disabled token', async function () {
+    it('Create token by providing name, userId, null date, null siteKey, DISABLED state', async function () {
         const client = apolloClient()
         const name = 'test-' + new Date().getTime()
-        const token = await createToken('root', name, 'DISABLED', null, client)
+
+        const response = await apolloClient().query({
+            query: GQL_CREATE,
+            variables: {
+                userId: 'root',
+                tokenName: name,
+                siteKey: null,
+                expireAt: null,
+                tokenState: 'DISABLED',
+            },
+        })
+        expect(response.errors).to.be.undefined
+
+        const token = response.data.admin.personalApiTokens.createToken
 
         expect(token).to.have.length.of.at.least(10, 'not a token')
         expect(await verifyToken(token, client)).to.be.false
@@ -35,59 +69,255 @@ describe('token creation', () => {
         const tokenDetails = await getToken('root', name, client)
         expect(tokenDetails).to.not.be.null
         expect(tokenDetails.name).to.equals(name)
+        expect(tokenDetails.state).to.equals('DISABLED')
     })
 
-    it('should create an expired token', async function () {
+    it('Create token by providing name, userId, null date, null siteKey, INCORRECT state (INACTIVE)', async function () {
         const client = apolloClient()
         const name = 'test-' + new Date().getTime()
-        const token = await createToken('root', name, null, '2019-01-01', client)
 
+        const response = await apolloClient().query({
+            query: GQL_CREATE,
+            variables: {
+                userId: 'root',
+                tokenName: name,
+                siteKey: null,
+                expireAt: null,
+                tokenState: 'INACTIVE', // This state does not exist
+            },
+            errorPolicy: 'ignore'
+        })
+        expect(response.data).to.be.null
+
+        const tokenDetails = await getToken('root', name, client)
+        expect(tokenDetails).to.be.null
+    })
+
+    it('Create token by providing userId, name, expiry date, null state, null siteKey', async function () {
+        const client = apolloClient()
+        const name = 'test-' + new Date().getTime()
+        const expireAt = '2040-01-01'
+
+        const response = await apolloClient().query({
+            query: GQL_CREATE,
+            variables: {
+                userId: 'root',
+                tokenName: name,
+                siteKey: null,
+                expireAt: expireAt,
+                tokenState: null,
+            },
+        })
+        expect(response.errors).to.be.undefined
+
+        const token = response.data.admin.personalApiTokens.createToken
+        expect(token).to.have.length.of.at.least(10, 'not a token')
+        expect(await verifyToken(token, client)).to.be.true
+
+        const tokenDetails = await getToken('root', name, client)
+        expect(tokenDetails).to.not.be.null
+        expect(tokenDetails.state).to.equals('ACTIVE')
+        // Using slice prevent us from having to deal with timezones (hopefully)
+        expect(tokenDetails.expireAt.slice(0, 10)).to.equals(expireAt) 
+    })
+
+    it('Create token by providing userId, name, expiry date in the past, null state, null siteKey', async function () {
+        const client = apolloClient()
+        const name = 'test-' + new Date().getTime()
+        const expireAt = '2010-01-01'
+
+        const response = await apolloClient().query({
+            query: GQL_CREATE,
+            variables: {
+                userId: 'root',
+                tokenName: name,
+                siteKey: null,
+                expireAt: expireAt,
+                tokenState: null,
+            },
+        })
+        expect(response.errors).to.be.undefined
+
+        const token = response.data.admin.personalApiTokens.createToken
         expect(token).to.have.length.of.at.least(10, 'not a token')
         expect(await verifyToken(token, client)).to.be.false
 
         const tokenDetails = await getToken('root', name, client)
         expect(tokenDetails).to.not.be.null
-        expect(tokenDetails.name).to.equals(name)
+        expect(tokenDetails.state).to.equals('ACTIVE')
+        expect(tokenDetails.expireAt.slice(0, 10)).to.equals(expireAt) 
     })
 
-    it('should not create 2 tokens with the same name', async function () {
+    it('Create token by providing userId, name, INCORRECT expiry date, null state, null siteKey', async function () {
         const client = apolloClient()
         const name = 'test-' + new Date().getTime()
-        const token = await createToken('root', name, null, '2019-01-01', client)
-        let error
-        try {
-            await createToken('root', name, null, '2019-01-02', client)
-        } catch (e) {
-            error = e
-        }
+        const expireAt = '2010-ABCDEF-01'
 
+        const response = await apolloClient().query({
+            query: GQL_CREATE,
+            variables: {
+                userId: 'root',
+                tokenName: name,
+                siteKey: null,
+                expireAt: expireAt,
+                tokenState: null,
+            },
+            errorPolicy: 'ignore'
+        })
+        expect(response.data.admin.personalApiTokens.createToken).to.be.null
+
+        const tokenDetails = await getToken('root', name, client)
+        expect(tokenDetails).to.be.null
+    })
+
+    it('Create token by providing userId, name, EMPTY expiry date, null state, null siteKey', async function () {
+        const client = apolloClient()
+        const name = 'test-' + new Date().getTime()
+        const expireAt = ''
+
+        const response = await apolloClient().query({
+            query: GQL_CREATE,
+            variables: {
+                userId: 'root',
+                tokenName: name,
+                siteKey: null,
+                expireAt: expireAt,
+                tokenState: null,
+            },
+            errorPolicy: 'ignore'
+        })
+        expect(response.data.admin.personalApiTokens.createToken).to.be.null
+
+        const tokenDetails = await getToken('root', name, client)
+        expect(tokenDetails).to.be.null
+    })
+
+    it('Create same name token by providing userId, name, expiry date, null state, null siteKey', async function () {
+        const client = apolloClient()
+        const name = 'test-' + new Date().getTime()
+        const expireAt = '2040-01-01'
+
+        let response = await apolloClient().query({
+            query: GQL_CREATE,
+            variables: {
+                userId: 'root',
+                tokenName: name,
+                siteKey: null,
+                expireAt: expireAt,
+                tokenState: null,
+            },
+        })
+        expect(response.errors).to.be.undefined
+
+        const token = response.data.admin.personalApiTokens.createToken
         expect(token).to.have.length.of.at.least(10, 'not a token')
-        expect(error).to.not.be.null
-    })
+        expect(await verifyToken(token, client)).to.be.true
 
-    it('should not create a tokens invalid user', async function () {
+        let tokenDetails = await getToken('root', name, client)
+        expect(tokenDetails).to.not.be.null
+        expect(tokenDetails.name).to.equals(name)
+
+        response = await apolloClient().query({
+            query: GQL_CREATE,
+            variables: {
+                userId: 'root',
+                tokenName: name,
+                siteKey: null,
+                expireAt: '2019-01-01',
+                tokenState: null,
+            },
+            errorPolicy: 'ignore'
+        })     
+        expect(response.data.admin.personalApiTokens.createToken).to.be.null
+   
+        tokenDetails = await getToken('root', name, client)
+        expect(tokenDetails).to.not.be.null
+        expect(tokenDetails.name).to.equals(name)
+        expect(tokenDetails.expireAt.slice(0, 10)).to.equals(expireAt) 
+
+    })    
+
+    it('Create token by providing EMPTY userId, name, null date, null state, null siteKey', async function () {
         const client = apolloClient()
         const name = 'test-' + new Date().getTime()
-        let error
-        try {
-            await createToken('rootxxxx', name, null, '2019-01-01', client)
-        } catch (e) {
-            error = e
-        }
 
-        expect(error).to.not.be.null
+
+        const response = await apolloClient().query({
+            query: GQL_CREATE,
+            variables: {
+                userId: '',
+                tokenName: name,
+                siteKey: null,
+                expireAt: null,
+                tokenState: null,
+            },
+            errorPolicy: 'ignore'
+        })
+        expect(response.data.admin.personalApiTokens.createToken).to.be.null
+        const tokenDetails = await getToken('root', name, client)
+        expect(tokenDetails).to.be.null
     })
 
-    it('should not create a tokens invalid date', async function () {
+    it('Create token by providing NON-EXISTING userId, name, null date, null state, null siteKey', async function () {
         const client = apolloClient()
         const name = 'test-' + new Date().getTime()
-        let error
-        try {
-            await createToken('root', name, null, '2019-0sadasd1-01', client)
-        } catch (e) {
-            error = e
-        }
 
-        expect(error).to.not.be.null
-    })
+        const response = await apolloClient().query({
+            query: GQL_CREATE,
+            variables: {
+                userId: 'I_DO_NOT_EXIST',
+                tokenName: name,
+                siteKey: null,
+                expireAt: null,
+                tokenState: null,
+            },
+            errorPolicy: 'ignore'
+        })
+        expect(response.data.admin.personalApiTokens.createToken).to.be.null
+        const tokenDetails = await getToken('root', name, client)
+        expect(tokenDetails).to.be.null        
+    })    
+
+    it('Create token by providing userId, NULL name, null date, null state, null siteKey', async function () {
+        const client = apolloClient()
+        const name = null
+
+        const response = await apolloClient().query({
+            query: GQL_CREATE,
+            variables: {
+                userId: 'root',
+                tokenName: name,
+                siteKey: null,
+                expireAt: null,
+                tokenState: null,
+            },
+            errorPolicy: 'ignore'
+        })
+        cy.log(JSON.stringify(response))
+
+        expect(response.data).to.be.null
+    })        
+
+    it('Create token by providing userId, EMPTY name, null date, null state, null siteKey', async function () {
+        const client = apolloClient()
+        const name = ''
+
+        const response = await apolloClient().query({
+            query: GQL_CREATE,
+            variables: {
+                userId: 'root',
+                tokenName: name,
+                siteKey: null,
+                expireAt: null,
+                tokenState: null,
+            },
+            errorPolicy: 'ignore'
+        })
+        cy.log(JSON.stringify(response))
+        expect(response.data.admin.personalApiTokens.createToken).to.be.null
+
+        const tokenDetails = await getToken('root', name, client)
+        expect(tokenDetails).to.be.null        
+    })    
+
 })
