2c5c9cda9ee73db6ca4631f08b06bef9f0344803
cedmail <cmailleux@jahia.com>
2020-11-19 09:58:23 -0500

    BACKLOG-14906: Allow to disable/activate a token

--- src/main/javascript/PersonalApiTokens/TokensList/TokensList.gql.jsx
+++ src/main/javascript/PersonalApiTokens/TokensList/TokensList.gql.jsx
@@ -1,45 +1,55 @@
 import gql from 'graphql-tag';
 
 const getTokens = gql`
-   query GetTokens($userId: String, $site: String, $before: String, $after: String, 
-   $first: Int, $last: Int, $offset: Int, $limit: Int, $fieldSorter: InputFieldSorterInput) {
+    query GetTokens($userId: String, $site: String, $before: String, $after: String,
+        $first: Int, $last: Int, $offset: Int, $limit: Int, $fieldSorter: InputFieldSorterInput) {
         admin {
-          personalApiTokens {
-            tokens(userId: $userId, site: $site, before: $before, after: $after, first: $first, 
-            last: $last, offset: $offset, limit: $limit, fieldSorter: $fieldSorter) {
-              pageInfo {
-                totalCount
-                nodesCount
-                startCursor
-                endCursor
-                hasNextPage
-                hasPreviousPage
-              }
-              nodes {
-                name
-                key
-                expireAt
-                state
-                createdAt
-                updatedAt
-                user {
-                    name
+            personalApiTokens {
+                tokens(userId: $userId, site: $site, before: $before, after: $after, first: $first,
+                    last: $last, offset: $offset, limit: $limit, fieldSorter: $fieldSorter) {
+                    pageInfo {
+                        totalCount
+                        nodesCount
+                        startCursor
+                        endCursor
+                        hasNextPage
+                        hasPreviousPage
+                    }
+                    nodes {
+                        name
+                        key
+                        expireAt
+                        state
+                        createdAt
+                        updatedAt
+                        user {
+                            name
+                        }
+                    }
                 }
-              }
-            }
-          }
-        }
-     }
-`;
-
-const DeleteTokenMutation = gql`
-    mutation DeleteToken($key: String!) {
-        admin {
-             personalApiTokens {
-                 deleteToken(key: $key)
             }
         }
     }
 `;
 
-export {DeleteTokenMutation, getTokens};
+const DeleteTokenMutation = gql`
+    mutation DeleteToken($key: String!) {
+        admin {
+            personalApiTokens {
+                deleteToken(key: $key)
+            }
+        }
+    }
+`;
+
+const StateTokenMutation = gql`
+    mutation DeleteToken($key: String!, $state: TokenState) {
+        admin {
+            personalApiTokens {
+                updateToken(key: $key, state: $state)
+            }
+        }
+    }
+`;
+
+export {DeleteTokenMutation, getTokens, StateTokenMutation};
