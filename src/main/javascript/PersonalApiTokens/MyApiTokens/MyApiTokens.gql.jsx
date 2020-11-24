8eca7d8e3a3880d32ee18c8bd924aab6f9d1f152
Thomas Draier <tdraier@jahia.com>
2020-11-24 12:07:54 +0100

    Fixed creation with empty date

--- src/main/javascript/PersonalApiTokens/MyApiTokens/MyApiTokens.gql.jsx
+++ src/main/javascript/PersonalApiTokens/MyApiTokens/MyApiTokens.gql.jsx
@@ -1,7 +1,7 @@
 import gql from 'graphql-tag';
 
 const CreateTokenMutation = gql`
-    mutation CreateToken($name: String!, $expireAt: String!) {
+    mutation CreateToken($name: String!, $expireAt: String) {
         admin {
              personalApiTokens {
                  createToken(name: $name, expireAt: $expireAt)
