657b2c94f9fa26b97faec351da7470b93b1493e6
Thomas Draier <tdraier@jahia.com>
2020-11-24 09:42:54 +0100

    Removed unused code

--- src/test/java/org/jahia/modules/apitoken/TokenUtilsTest.java
+++ src/test/java/org/jahia/modules/apitoken/TokenUtilsTest.java
@@ -10,7 +10,6 @@ public class TokenUtilsTest {
     public void testToken() {
         TokenUtils tokenUtils = TokenUtils.getInstance();
         String token = tokenUtils.generateToken();
-        assertTrue("Invalid key format", tokenUtils.checkKeyFormat(tokenUtils.getKey(token)));
         assertEquals("Invalid secret", 16, tokenUtils.getSecret(token).length);
     }
 }
