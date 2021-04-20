b94b2f02368aa59b4354593d316df0a7289e0ae1
Francois Gerthoffert <f.gerthoffert@gmail.com>
2021-04-20 09:39:14 -0400

    Added some logging

--- tests/cypress/integration/userTokensE2E.spec.ts
+++ tests/cypress/integration/userTokensE2E.spec.ts
@@ -22,6 +22,7 @@ describe('UI e2e test - Full lifecycle in the User API Tokens section in Adminis
 
     it('Creates sample tokens via the API', async () => {
         const token = await createToken(`test-token`, 'ACTIVE', null, apolloClient())
+        cy.log(JSON.stringify(token))
         expect(token).to.have.length.of.at.least(10, 'not a token')
     })
 
--- tests/cypress/plugins/index.js
+++ tests/cypress/plugins/index.js
@@ -4,6 +4,8 @@ const cypressTypeScriptPreprocessor = require("./cy-ts-preprocessor");
 module.exports = (on, config) => {
     // eslint-disable-next-line @typescript-eslint/no-var-requires
     require("@cypress/code-coverage/task")(on, config);
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    require('cypress-terminal-report/src/installLogsPrinter')(on);
     on("file:preprocessor", cypressTypeScriptPreprocessor);
     return config;
 };
