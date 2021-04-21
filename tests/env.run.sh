4c502c1520270ce48cee89dd6fafdc1c43756d37
Francois Gerthoffert <f.gerthoffert@gmail.com>
2021-04-20 20:46:25 -0400

    Trying to understand why digitall user is invalid

--- tests/env.run.sh
+++ tests/env.run.sh
@@ -38,6 +38,9 @@ if [[ ${MANIFEST} == *"build"* ]]; then
   echo "$(date +'%d %B %Y - %k:%M') == Module submitted =="
 fi
 
+# TO be removed
+sleep 45
+
 echo "$(date +'%d %B %Y - %k:%M') == Warming up the environement =="
 curl -v -u root:${SUPER_USER_PASSWORD} -X POST ${JAHIA_URL}/modules/api/provisioning --form script="@warmup-manifest-build.yaml;type=text/yaml" --form file="@assets/createToken.groovy"
 echo "$(date +'%d %B %Y - %k:%M') == Environment warmup complete =="
