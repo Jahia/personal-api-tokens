241b6870a92bfbb0f10db6e027e6d86b060c0b39
Janin-Michel-Mathias <jmichelmathias@jahia.com>
2025-08-21 11:11:03 +0200

    fix: align title

--- src/main/javascript/PersonalApiTokens/ScopesSelector/ScopesSelector.jsx
+++ src/main/javascript/PersonalApiTokens/ScopesSelector/ScopesSelector.jsx
@@ -12,7 +12,6 @@ const columns = [
         Header: header => <Checkbox isUncontrolled {...header.getToggleAllRowsSelectedProps()}/>,
         Cell: cellInfo => <Checkbox isUncontrolled {...cellInfo.row.getToggleRowSelectedProps()}/>,
         width: 52,
-        canResize: false
     },
     {
         id: 'name',
